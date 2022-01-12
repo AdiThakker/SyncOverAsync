fad80c12690e9ea174203f0769de1fbd3ffa832a
AdiThakker <Aditya.Thakker@gmail.com>
2022-01-12 14:02:07 -0500

    started adding swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.OpenApi.Models;
+
+// Create builder
 var builder = WebApplication.CreateBuilder(args);
+
+// register services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(_ => _.SwaggerDoc("V1", new OpenApiInfo { Title = "Sync Over Async API", Description = "Example showing use of TCS to control task completion", Version = "V1" }));
+
+// build
 var app = builder.Build();
+
+
+// use services
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sync Over Async API V1"));
 
 app.MapGet("/", () => "Hello World!");
 
