298b5afb0c6f0a5156201a8fd4b8adeb1cbcb5f2
AdiThakker <Aditya.Thakker@gmail.com>
2022-01-13 14:34:32 -0500

    removed data Store and models and got a basic sample with TCS working

--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using SyncOverAsync.DataStore;
-using SyncOverAsync.Models;
+using System.Collections.Concurrent;
 
 // Create builder
 var builder = WebApplication.CreateBuilder(args);
 
 // register services
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<WeatherDb>(options => options.UseInMemoryDatabase("weather"));
 builder.Services.AddSwaggerGen(_ => _.SwaggerDoc("v1", new OpenApiInfo { Title = "Sync Over Async API", Description = "Example showing use of TCS to control task completion", Version = "v1" }));
 
 // build
@@ -18,15 +15,63 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sync Over Async API V1"));
 
-// Add routes
-app.MapGet("/weatherforecastSync", (WeatherDb db) => db.GetWeather());
+// Add Routes
+app.MapGet("/weatherforecastsync", () => WeatherForecast.GetWeather());
 
-// Initialize Data
-var weatherDb = app.Services.GetService<WeatherDb>();
-var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-weatherDb?.Weather.AddRange(Enumerable.Range(1, 5).Select(index => new WeatherForecast(index, Random.Shared.Next(-20, 55), summaries[Random.Shared.Next(summaries.Length)])).ToArray());
-weatherDb?.SaveChanges();
-
+app.MapGet("/weatherforecastasync", async () => await WeatherForecast.GetWeatherAsync(Random.Shared.Next(int.MaxValue).ToString()));
 
 // Run
 app.Run();
+
+// Data
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    private static string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public static WeatherForecast[] GetWeather()
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+        return forecast;
+    }
+
+    static ConcurrentDictionary<string, TaskCompletionSource<WeatherForecast[]>> requests = new ConcurrentDictionary<string, TaskCompletionSource<WeatherForecast[]>>();
+
+    public static async Task<WeatherForecast[]> GetWeatherAsync(string requestId)
+    {
+        TaskCompletionSource<WeatherForecast[]> tcs = new TaskCompletionSource<WeatherForecast[]>(requestId);
+        requests.TryAdd(requestId, tcs);
+        GetWeatherAsyncCompletion(requestId);
+       return await tcs.Task;
+    }
+
+    private static async Task GetWeatherAsyncCompletion(string requestId)
+    {
+        // simulate background process such as listening on Service Bus topic / external call back, etc.
+        await Task.Delay(10000);
+
+        // Get the tcs that matches the requestId
+        if (requests.TryGetValue(requestId, out TaskCompletionSource<WeatherForecast[]> tcs))
+        {
+            tcs.SetResult(GetWeather());
+        }
+
+    }
+
+}
+
+
+
+
+
