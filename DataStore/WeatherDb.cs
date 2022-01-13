8c538471448434d15add718824c2c8275a987bcb
AdiThakker <Aditya.Thakker@gmail.com>
2022-01-13 10:38:40 -0500

    started adding EF Core in-memory elements

---
+++ DataStore/WeatherDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncOverAsync.Models;
+
+namespace SyncOverAsync.DataStore
+{
+    public class WeatherDb : DbContext
+    {
+        public WeatherDb(DbContextOptions options) : base(options) { }
+
+        public DbSet<WeatherForecast> Weather { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using SyncOverAsync.DataStore;
 
 // Create builder
 var builder = WebApplication.CreateBuilder(args);
 
 // register services
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(_ => _.SwaggerDoc("V1", new OpenApiInfo { Title = "Sync Over Async API", Description = "Example showing use of TCS to control task completion", Version = "V1" }));
+builder.Services.AddDbContext<WeatherDb>(options => options.UseInMemoryDatabase("weather"));
+builder.Services.AddSwaggerGen(_ => _.SwaggerDoc("v1", new OpenApiInfo { Title = "Sync Over Async API", Description = "Example showing use of TCS to control task completion", Version = "v1" }));
 
 // build
 var app = builder.Build();
-
 
 // use services
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sync Over Async API V1"));
 
-app.MapGet("/", () => "Hello World!");
+// Add routes
+app.MapGet("/", () => "Hello Sync Over Async Web API!");
+app.MapGet("/weatherforecast", async (WeatherDb db) => await db.Weather.ToListAsync());
 
 app.Run();
